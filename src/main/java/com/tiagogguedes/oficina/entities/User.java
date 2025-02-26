fdfc25b2c5d394e3688f8a251e28a3ccd7c13507
Tiago Guedes <tiagogguedes@gmail.com>
2025-02-26 14:06:34 -0300

    H2 database, test profile, JPA

--- src/main/java/com/tiagogguedes/oficina/entities/User.java
+++ src/main/java/com/tiagogguedes/oficina/entities/User.java
@@ -3,11 +3,21 @@ package com.tiagogguedes.oficina.entities;
 import java.io.Serializable;
 import java.util.Objects;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable{
 	
 	
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String email;
@@ -19,7 +29,7 @@ public class User implements Serializable{
 
 
 	public User(Long id, String name, String email, String phone, String password) {
-		super();
+		
 		this.id = id;
 		this.name = name;
 		this.email = email;
--- src/main/java/com/tiagogguedes/oficina/resources/UserResource.java
+++ src/main/java/com/tiagogguedes/oficina/resources/UserResource.java
@@ -7,10 +7,15 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.tiagogguedes.oficina.entities.User;
 
+//um controlador RESTful, indica que os métodos da classe retornam dados 
+//diretamente em formato JSON ou XML, sem renderizar uma página HTML.
 @RestController
+//usada para mapear solicitações HTTP
 @RequestMapping(value = "/users")
 public class UserResource {
 	
+	//é um atalho para @RequestMapping(method = RequestMethod.GET) e é usada especificamente 
+	//para mapear solicitações HTTP GET
 	@GetMapping
 	public ResponseEntity<User> findAll(){
 		
